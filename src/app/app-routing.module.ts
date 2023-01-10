982dd60592df844bb7ea826182fa0e8bc276bf43
MayorlenO <mayorlen.ortega@gmail.com>
2023-01-10 17:47:43 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path:'404',
+    component: ErrorPageComponent
+  },
+  {
+    path:'**',
+    component: ErrorPageComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
